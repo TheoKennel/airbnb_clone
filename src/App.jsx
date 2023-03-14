2e21f30a664f920e9bbf2d4fb87227c8fab44c85
TheoKennel <theokennel@gmail.com>
2023-03-15 00:31:30 +0100

    Rajout style + couleur bouton search

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <div>Une semaine</div>
           <div className="border border-l border-gray-300"></div>
           <div>Ajouter des voyageurs</div>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <button className="bg-primary text-white p-2 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </button>
