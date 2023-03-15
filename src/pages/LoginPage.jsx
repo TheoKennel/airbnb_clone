5c37042807a6c555649db7a5d3d57e597a50613a
TheoKennel <theokennel@gmail.com>
2023-03-16 00:46:48 +0100

    Login Page : Début formulaire connection

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,9 +3,13 @@ import Header from "../Content/Header.jsx";
 
 const LoginPage = () => {
     return  (
-        <div>
-            LALALA
-
+        <div className="mt-4">
+            <h1 className="text-4xl text-center pb-1">Login</h1>
+            <form className="max-w-lg mx-auto border">
+                <input type="email" placeholder='your@email.com'/>
+                <input type="Mot de passe" placeholder='Mot de passe'/>
+                <button>Login</button>
+            </form>
         </div>
     );
 };
