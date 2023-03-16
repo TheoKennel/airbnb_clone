bd9e3452d41a7a74e572041a609be0d76cd9257f
TheoKennel <theokennel@gmail.com>
2023-03-16 11:30:55 +0100

    Ajout texte + link s'inscrire login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "../Content/Header.jsx";
+import {Link} from "react-router-dom";
 
 const LoginPage = () => {
     return  (
@@ -10,6 +11,9 @@ const LoginPage = () => {
                     <input type="email" placeholder='your@email.com'/>
                     <input type="Mot de passe" placeholder='Mot de passe'/>
                     <button className="primary">Login</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Vous n'avez pas encore de compte ? <Link className="underline text-black" to={'./register'}> Inscrivez-vous</Link>
+                    </div>
                 </form>
             </div>
         </div>
