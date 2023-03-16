4ee6b04b4fdd4f5980288fa921b15d2856a99e72
TheoKennel <theokennel@gmail.com>
2023-03-16 11:24:28 +0100

    style, login fini, centrage ok manque responsive

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,13 +3,15 @@ import Header from "../Content/Header.jsx";
 
 const LoginPage = () => {
     return  (
-        <div className="mt-4">
-            <h1 className="text-4xl text-center pb-1">Login</h1>
-            <form className="max-w-lg mx-auto border">
-                <input type="email" placeholder='your@email.com'/>
-                <input type="Mot de passe" placeholder='Mot de passe'/>
-                <button>Login</button>
-            </form>
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64 pl-9">
+                <h1 className="text-4xl text-center pb-1">Login</h1>
+                <form className="max-w-lg mx-auto">
+                    <input type="email" placeholder='your@email.com'/>
+                    <input type="Mot de passe" placeholder='Mot de passe'/>
+                    <button className="primary">Login</button>
+                </form>
+            </div>
         </div>
     );
 };
