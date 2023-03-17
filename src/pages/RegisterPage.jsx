ed760844e5b95df427c8d131f2f9f1bc224e5e23
TheoKennel <theokennel@gmail.com>
2023-03-17 17:59:57 +0100

    Cont pour name email password + stockage valeur

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -3,12 +3,15 @@ import Header from "../Content/Header.jsx";
 import {Link} from "react-router-dom";
 
 const RegisterPage = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return  (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64 pl-9">
                 <h1 className="text-4xl text-center pb-1">Register</h1>
                 <form className="max-w-lg mx-auto">
-                    <input type="text" placeholder="Théo Kosv"/>
+                    <input type="text" placeholder="Théo Kosv" value={name} onChange={ev => setName(ev.target.value)}/>
                     <input type="email" placeholder='your@email.com'/>
                     <input type="Mot de passe" placeholder='Mot de passe'/>
                     <button className="primary">Login</button>
