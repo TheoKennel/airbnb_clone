44061be1fdbfc01294f0bb6bdb85038e0e31f7fe
TheoKennel <theokennel@gmail.com>
2023-03-18 19:54:12 +0100

    Mise en place cors + function cors ok, test dans la console ok

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 
+app.use(cors({
+    credentials:true,
+    origin:'http://http://localhost:5173',
+}));
+
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
