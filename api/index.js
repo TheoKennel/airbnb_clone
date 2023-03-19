09990b7e01af1b861b2de8f156dc9512f2be3cad
TheoKennel <theokennel@gmail.com>
2023-03-19 15:36:00 +0100

    Ajout try catch pour les données name email password + error + modofication input password + ajout async await registerUser

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import UserModel from "./models/User.js";
+import bcrypt from 'bcryptjs';
 dotenv.config();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 app.use(express.json());
@@ -12,6 +16,7 @@ app.use(cors({
     origin:'http://localhost:5173',
 }));
 
+
 mongoose.connect(process.env.MONGO_URL);
 
 
@@ -19,9 +24,18 @@ app.get('/test', (req,res) => {
     res.json('test ok');
 });
 
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
     const {name,email,password} = req.body;
-    res.json({name,email,password});
+    try {
+        const userDoc = await UserModel.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
 });
 
 
