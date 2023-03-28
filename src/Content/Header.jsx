448ff49caf757290115a8df61dc16877b56d2a05
Théo <theokennel@gmail.com>
2023-03-29 00:08:46 +0200

    Problème balise + et = manquant header.jsx, corrigé

--- src/Content/Header.jsx
+++ src/Content/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
      const {user} = useContext(UserContext);
     return (
         <header className="flex justify-between">
-            <Link to{'/'} className="flex items-center gap-1">
+            <Link to={'/'} className="flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                      stroke="currentColor" className="w-8 h-8 -rotate-90" color="#f87171">
                     <path strokeLinecap="round" strokeLinejoin="round"
                           d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"/>
                 </svg>
                 <span className="font-bold text-xl" style={{color: '#f87171'}}>Airbnb_Clone</span>
-            </>
+            </Link>
             <div className="flex  gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
                 <div>N'importe où</div>
                 <div className="border border-l border-gray-300"></div>
