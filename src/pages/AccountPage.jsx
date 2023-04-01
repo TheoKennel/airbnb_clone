0021281b823ede8b6405e218869f88185866faf1
Théo <theokennel@gmail.com>
2023-04-01 18:39:34 +0200

    ajout function linkClasses (style par link)

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -13,15 +13,25 @@ const AccountPage = () => {
         return <Navigate to={'/login'}/>
     }
 
-    const {subpage} =  useParams();
+     let {subpage} =  useParams();
+    if(subpage === undefined) {
+        subpage = 'profile'
+    }
 
+    const linkClasses = (type=null) => {
+        let classes =  'p-2 px-6'
+        if( type === subpage || (subpage === undefined && type === 'profile')) {
+            classes += 'bg-primary text-white rounded-full'
+        }
+        return classes;
+    }
 
     return (
         <div>
             <nav className="w-full flex justify-center mt-8 gap-2">
-                <Link className="p-2 px-6 bg-primary text-white rounded-full" to={'/account'}>My profile</Link>
-                <Link className="p-2 px-6" to={'/account/bookings'}>My bookings</Link>
-                <Link  className="p-2 px-6" to={'/account/places'}>My accommodations</Link>
+                <Link className={linkClasses('profile')} to={'/account'}>My profile</Link>
+                <Link className={linkClasses('bookings')} to={'/account/bookings'}>My bookings</Link>
+                <Link  className={linkClasses('places')} to={'/account/places'}>My accommodations</Link>
             </nav>
         </div>
     );
